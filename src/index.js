76cbc1648ea5f9b0d6acd3d1c0e4e2789ebd4a6a
Igor Postolenko <igorpost92@yandex.ru>
2018-05-18 19:02:33 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -67,8 +67,3 @@ const genDiff = (first, second, format = defaultFormat) => {
 };
 
 export default genDiff;
-
-// const b = '/Users/igor/Documents/Dev/Hexlet/project2/__tests__/__fixtures__/before.ini';
-// const a = '/Users/igor/Documents/Dev/Hexlet/project2/__tests__/__fixtures__/after.ini';
-
-// console.log(genDiff(b, a));
